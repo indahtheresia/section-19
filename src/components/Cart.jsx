96aff028964c1e1d020d2ce31891cafbba3d3fc5
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-17 16:36:36 +0800

    Work on the cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import Button from "../UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -19,7 +20,9 @@ export default function Cart() {
     <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
       <h2>Your Cart</h2>
       <ul>
-        {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
+        {cartCtx.items.map(item => (
+          <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price} onDecrease={() => cartCtx.removeItem(item.id)} onIncrease={() => cartCtx.addItem(item)} />
+        ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
